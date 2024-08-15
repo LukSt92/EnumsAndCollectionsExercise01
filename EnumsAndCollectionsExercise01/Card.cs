8a8174977774f31684fdfdfcf56779bd093cb9f5
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-08-15 12:57:04 +0200

    Add: Card class

---
+++ EnumsAndCollectionsExercise01/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsAndCollectionsExercise01
+{
+    class Card
+    {
+        public string Value { get; set; }
+        public string Suit { get; set; }
+        public string Name { get { return $"{Value} of {Suit}"; } }
+    }
+}
