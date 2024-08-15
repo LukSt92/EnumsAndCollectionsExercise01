9fae55037404805b3167b1d32c2a979d59720e17
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-08-15 13:05:39 +0200

    Add: Constructor to Class card

--- EnumsAndCollectionsExercise01/Card.cs
+++ EnumsAndCollectionsExercise01/Card.cs
@@ -8,8 +8,14 @@ namespace EnumsAndCollectionsExercise01
 {
     class Card
     {
-        public string Value { get; set; }
-        public string Suit { get; set; }
+        public Card(string value, string suit)
+        {
+            this.Value = value;
+            this.Suit = suit;
+        }
+
+        public string Value { get; private set; }
+        public string Suit { get; private set; }
         public string Name { get { return $"{Value} of {Suit}"; } }
     }
 }
